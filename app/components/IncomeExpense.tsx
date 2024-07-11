ea6c98072c848939012632041008a9a6fa44ede4
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-07-11 17:31:30 +0100

    list component added

--- app/components/IncomeExpense.tsx
+++ app/components/IncomeExpense.tsx
@@ -1,13 +1,19 @@
-const IncomeExpense = () => {
+import { addCommas } from "@/lib/utils";
+import getIncomeExpense from "../actions/getIncomeExpenses";
+
+const IncomeExpense = async () => {
+  const { income, expense } = await getIncomeExpense();
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">$700</p>
+        <p className="money plus">${addCommas(Number(income?.toFixed(2)))}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$600</p>
+        <p className="money minus">
+          -${addCommas(Number(expense?.toFixed(2)))}
+        </p>
       </div>
     </div>
   );
